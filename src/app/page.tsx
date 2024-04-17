81661f5ee84b31290b080978cf8d06271570913b
Kevin Juan Jacque Castillo <kevin.jacque@alumnos.ucentral.cl>
2024-04-17 10:17:41 -0400

    Refactor page.tsx and add Table component to display results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,19 @@ import { createCharacter, getCharacter } from "@/actions/form.character";
 import { useFormState } from "react-dom";
 import { ContentForm } from "./components/content.input";
 import { ShowResult } from "./components/show.result";
+import React, { useState } from "react";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function HomePage() {
+  const [Resultados, setResultados] = useState<string[]>([])
   
   const handleSumbit =async (state:unknown,formdata:FormData)=>{
     
@@ -15,6 +26,7 @@ export default function HomePage() {
     while(["starting","processing"].includes(character.status)){
       character = await getCharacter(id);
       console.log(character.id,id)
+      setResultados(prev=>[...prev,character.status])
       await new Promise((resolve) => setTimeout(resolve, 4000));
     }
     return character
@@ -28,5 +40,27 @@ export default function HomePage() {
       <form action={formAction} className="flex flex-col gap-2 py-5">
       <ContentForm />
     </form>
+
+    <Table className="table-auto">
+      <TableHeader>
+        <TableRow>
+          <TableHead>Intento</TableHead>
+          <TableHead>Result</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell>example</TableCell>
+          <TableCell>expample</TableCell>
+        </TableRow>
+        {Resultados.map((result, index) => (
+        <React.Fragment key={index+result}>
+          <TableRow>
+          <TableCell>{index}</TableCell>
+          <TableCell>{result}</TableCell>
+        </TableRow>
+        </React.Fragment>))}
+      </TableBody>
+    </Table>
   </section>;
 }
